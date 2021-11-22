b8946a4de6e98b8138728fb17e1c7cba48b29914
Török Attila <ifj.torok.attila@gmail.com>
2021-11-22 09:21:45 +0100

    Create Proxy_node.py

---
+++ Proxy_node.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+
+# Software License Agreement (BSD License)
+
+#
+
+# Copyright (c) 2008, Willow Garage, Inc.
+
+# All rights reserved.
+
+#
+
+# Redistribution and use in source and binary forms, with or without
+
+# modification, are permitted provided that the following conditions
+
+# are met:
+
+#
+
+#  * Redistributions of source code must retain the above copyright
+
+#    notice, this list of conditions and the following disclaimer.
+
+#  * Redistributions in binary form must reproduce the above
+
+#    copyright notice, this list of conditions and the following
+
+#    disclaimer in the documentation and/or other materials provided
+
+#    with the distribution.
+
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+
+#    contributors may be used to endorse or promote products derived
+
+#    from this software without specific prior written permission.
+
+#
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+
+# POSSIBILITY OF SUCH DAMAGE.
+
+#
+
+# Revision $Id$
+
+
+
+## Simple talker demo that published std_msgs/Strings messages
+
+## to the 'chatter' topic
+
+
+
+import rospy
+
+from std_msgs.msg import String
+
+
+
+import boto3
+
+from bottle import run, post, request, response, get, route
+
+import simplejson as json
+
+import thread
+
+import time
+
+import sys
+
+import requests
+
+
+
+def start_lambda(ip):
+
+    lambda_client = boto3.client(
+
+        'lambda',
+
+        aws_access_key_id="AKIA2X5CLV52DM422DIO",
+
+        aws_secret_access_key="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",
+
+        region_name='us-east-2'
+
+    )
+
+    response = lambda_client.invoke(
+
+      FunctionName='talker',
+
+      Payload=json.dumps({"say":"szia lajos", "ip":str(ip)})
+
+    )
+
+    print(response)
+
+
+
+def start_server(ip):
+
+    run(host=str(ip), port=5000, debug=True)
+
+
+
+@route('/', method = 'POST')
+
+def process():
+
+    global pub
+
+    pub.publish('szia')
+
+
+
+if __name__ == '__main__':
+
+    try:
+
+	pub = rospy.Publisher('chatter', String, queue_size=10)
+
+        rospy.init_node('talker', anonymous=True)
+
+
+
+        ip = str(requests.get('https://checkip.amazonaws.com').text.strip())
+
+        thread.start_new_thread( start_server, (ip, ) )	
+
+	time.sleep(5)
+
+	print('lambda started on ip ', ip)
+
+	start_lambda(ip)
+
+    except rospy.ROSInterruptException:
+
+        pass
